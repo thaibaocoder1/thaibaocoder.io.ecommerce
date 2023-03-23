9667d4caa604cfead6036cf9aca13bc07e7a492b
thaibaocoder1 <thaibaok4.1@gmail.com>
2023-03-23 18:50:18 +0700

    change 2

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -6,6 +6,6 @@ spans.forEach((span) => {
     currentPage = page;
     spans.forEach((s) => s.classList.remove("active"));
     span.classList.add("active");
-    window.location.href = `/ecommerceWebsiteThaiBao/products-${page}.html`;
+    window.location.href = `products-${page}.html`;
   });
 });
